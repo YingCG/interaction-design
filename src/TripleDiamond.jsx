9089195f2c5433be58c2011336a5aa9ebc7e6ddd
YingCG <yingchiugoh@gmail.com>
2023-09-08 21:07:39 +1200

    improved version

--- src/TripleDiamond.jsx
+++ src/TripleDiamond.jsx
@@ -16,38 +16,20 @@ function TripleDiamond() {
         Every interaction count, Create Better Experience...
       </div>
       <div className="container">
-        <div className="container-triple">
-          <h2 className="text-above">Research</h2>
+        {
+          contents.map((content, i) => i !== 0 && 
+          (<div className="container-triple">
+          <h2 className="text-above">{content.MenuItem}</h2>
           <div
             className="triple-diamond"
-            onClick={() => setInfo(contents[1])}
+            onClick={() => setInfo(contents[i])}
           >
             <div className="arrow-left"></div>
             <div className="arrow-right"></div>
           </div>
-        </div>
-
-        <div className="container-triple">
-          <h2 className="text-above">Experiment</h2>
-          <div
-            className="triple-diamond"
-            onClick={() => setInfo(contents[2])}
-          >
-            <div className="arrow-left"></div>
-            <div className="arrow-right"></div>
-          </div>
-        </div>
-
-        <div className="container-triple">
-          <h2 className="text-above">Testing</h2>
-          <div
-            className="triple-diamond"
-            onClick={() => setInfo(contents[3])}
-          >
-            <div className="arrow-left"></div>
-            <div className="arrow-right"></div>
-          </div>
-        </div>
+        </div>)
+          )
+        }
       </div>
       {!info.hide && <div className="information">
         <p>{info.Title}</p>
--- src/data.js
+++ src/data.js
@@ -5,14 +5,17 @@ const contents = [
       "Memento Media is a creative industry startup that empowers people to save life's stories. When a loved one dies, we also sadly loose their stories. Memento Media uses storytelling and oral history practices to redefine the way families discover, capture and celebrate family history withoutmulti-generational living family archives.",
   },
   {
+    MenuItem: "Research",
     Title: "Research",
     Description: "Assessment 1",
   },
   {
+    MenuItem: "Experiment",
     Title: "Experiment",
     Description: "Assessment 2",
   },
   {
+    MenuItem: "Testing",
     Title: "Prototype",
     Description: "Assessment 3.",
   },
